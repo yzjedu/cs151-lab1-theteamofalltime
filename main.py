53a118bfd9eef32fbbaae550f131e1ddaf536a67
ajduenomartinez <ajduenomartinez@loyola.edu>
2024-09-13 15:08:29 -0400

    Lab 1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-# Enter code here
-# Read the README for instructions.
-# You must write an algorithm and test cases first!
+# Programmers:  Hazel, Antonio
+# Course:  CS151, Zelalem Yalew
+# Due Date: 9/18/2024
+# Lab Assignment: 1
+# Problem Statem111: [describe what task this program performs]
+# Data In: [list the type of information your user input requests]
+# Data Out:  [list the type of information output with print]
+# Credits: [Is your code based on an example in the book, in class, or something else?]
