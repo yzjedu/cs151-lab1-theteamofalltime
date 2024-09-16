45c7f7099cb8edaf3a2466add59ecbc570564850
hazel <hoosborne@loyola.edu>
2024-09-16 10:01:26 -0400

    main.py

--- main.py
+++ main.py
@@ -10,13 +10,8 @@
 
 
 #Prompt user to enter Miles, Miles per Gallon, and Price per gallon
-miles_traveled = float(input("Enter miles you will travel: "))
-miles_per_gallon = float(input("Enter gas mileage of your vehicle: "))
-gas_price = float(input('Enter gas price: '))
 
 #Compute total cost using the variables above
-total_price = (miles_traveled / miles_per_gallon) * gas_price
 
 #Output total_price to user rounded to the second decimal point.
-print("The total cost of gas for your trip is: $", f'{total_price:.2f}',".")
 
