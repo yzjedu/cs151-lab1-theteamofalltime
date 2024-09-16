e2abce08d667f093be03227f9c39ac540a91173a
hazel <hoosborne@loyola.edu>
2024-09-16 12:31:11 -0400

    main.py

--- main.py
+++ main.py
@@ -6,12 +6,18 @@
 # to travel a certain distance.
 # Data In: Distance Traveled (in miles), Price per gallon, Miles per gallon
 # Data Out:  Cost of trip
-# Credits: [Is your code based on an example in the book, in class, or something else?]
+# Credits: Float codes from Zybooks assignment 1.
 
 
 #Prompt user to enter Miles, Miles per Gallon, and Price per gallon
+distance = float(input("Enter distance you will travel(in miles):" ))
+miles_per_gallon = float(input("Enter gas mileage:"))
+price_per_gallon = float(input("Enter gas price:"))
 
 #Compute total cost using the variables above
 
+total_cost = (distance / miles_per_gallon) * price_per_gallon
+
 #Output total_price to user rounded to the second decimal point.
 
+print("Your trip's total cost is $", f'{total_cost:.2f}', ".")
